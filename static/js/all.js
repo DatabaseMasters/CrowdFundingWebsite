cb53e98e6fa809c5f511f118851114ff3ae6cf09
Milena Sapunova <milena.sapunova@gmail.com>
2017-07-25 19:37:09 +0300

    Add footer

--- static/js/all.js
+++ static/js/all.js
@@ -1,4 +1,4 @@
 window.onload = function() {
     console.log('--- Client JS loaded ---');
-    document.body.innerHTML += 'Dynamically added';
+    // document.body.innerHTML += 'Dynamically added';
 };
