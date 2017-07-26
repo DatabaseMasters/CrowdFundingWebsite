06f83af271b45afa942d4b4028aea6d9e5f8487c
Nadezhda Hristova <epohster@gmail.com>
2017-07-26 12:37:26 +0300

    Reordered blocks in master files

--- app/routes/api.router.js
+++ app/routes/api.router.js
@@ -21,29 +21,47 @@ const attachRoutes = (app, data) => {
             return res.send(item);
         })
         .get('/', (req, res) => {
-            let { q, page, size } = req.query;
-            page = parseInt(page, 10) || 1;
-            size = parseInt(size, 10) || 10;
-
-            let result = data.items;
-            if (q) {
-                q = q.toLowerCase();
-                result = data.items.filter((item) => {
-                    return item.name.toLocaleLowerCase().includes(q);
+            let { category, filter } = req.query;
+            category = category || {};
+            filter = filter || {};
+            // TODO add filter by newest, popular, most funded
+            console.log(category);
+            data.projects.getAll({ category: category.toLowerCase() })
+                .then((projects) => {
+                    console.log(projects);
+                    res.render('items/projects', { model: projects }, (err, html) => {
+                        res.send(html);
+                    });
+                })
+                .catch((err) => {
+                    console.log('--- ERROR --- ' + err);
                 });
-            }
-            result = result.slice((page - 1) * size, page * size);
-            res.send(result);
         })
-        // test with Postaman
-        .post('/', (req, res) => {
-            const item = req.body;
-            item.id = data.items.length + 1;
-            // hash password
-            data.items.push(item);
-            res.status(201)
-                .send(item);
-        });
+        // Pagination
+        // .get('/', (req, res) => {
+        //     let { q, page, size } = req.query;
+        //     page = parseInt(page, 10) || 1;
+        //     size = parseInt(size, 10) || 10;
+
+    //     let result = data.items;
+    //     if (q) {
+    //         q = q.toLowerCase();
+    //         result = data.items.filter((item) => {
+    //             return item.name.toLocaleLowerCase().includes(q);
+    //         });
+    //     }
+    //     result = result.slice((page - 1) * size, page * size);
+    //     res.send(result);
+    // })
+    // test with Postaman
+    .post('/', (req, res) => {
+        const item = req.body;
+        item.id = data.items.length + 1;
+        // hash password
+        data.items.push(item);
+        res.status(201)
+            .send(item);
+    });
 
     app.use('/api/items', router);
 };
