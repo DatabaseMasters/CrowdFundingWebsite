d526a2bf288c63d40ddb8397948ccfaba6db2c10
Aleksandar Ikonomov <a.ikonomov17@gmail.com>
2017-07-26 19:40:42 +0300

    Implemented register and log in

--- config/server.config.js
+++ config/server.config.js
@@ -1,7 +1,9 @@
 const port = 3001;
 const connectionString = 'mongodb://localhost/crowdfunding-db';
+const sessionSecret = 'Purple unicorn';
 
 module.exports = {
     port,
     connectionString,
+    sessionSecret,
 };
