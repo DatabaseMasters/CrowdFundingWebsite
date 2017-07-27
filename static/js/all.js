b6aa6b6bad6df1b918f872683264720cdbff5821
Nadezhda Hristova <epohster@gmail.com>
2017-07-27 14:31:32 +0300

    Client categories - moved router instance from router.js to all.js

--- static/js/all.js
+++ static/js/all.js
@@ -1,54 +1,52 @@
-/* globals $ requester*/
+/* globals $ requester router*/
 window.onload = function() {
     console.log('--- Client JS loaded ---');
 };
 
 var descriptions = {
-    explore: 'Find projects which you can help now',
-    medical: 'Donate to alleviate suffering and restore lives of adults and children',
-    animals: 'Save cute pets or endangered species from suffering',
-    culture: 'Support art, social and cultural events or individuals',
-    other: 'Everything else you can be a hero for'
+    'explore projects': 'Find projects which you can help now',
+    'medical': 'Donate to alleviate suffering and restore lives of adults and children',
+    'animals': 'Save cute pets or endangered species from suffering',
+    'culture': 'Support art, social and cultural events or individuals',
+    'other': 'Everything else you can be a hero for'
 }
 
 var images = {
-    explore: '../static/images/children-bubbles.jpg',
-    medical: '../static/images/climber-sea.jpg',
-    animals: '../static/images/Climbers-shadow.jpg',
-    culture: '../static/images/FacetheSunShadowCseeker.jpg',
-    other: '../static/images/family-shadow-sea.jpg'
+    'explore projects': '../static/images/children-bubbles.jpg',
+    'medical': '../static/images/climber-sea.jpg',
+    'animals': '../static/images/Climbers-shadow.jpg',
+    'culture': '../static/images/FacetheSunShadowCseeker.jpg',
+    'other': '../static/images/family-shadow-sea.jpg'
 }
 
 // filter projects by selected category
 
-$('.dropdown-menu').click(function(event) {
-    var clickedId = event.target.id;
-    // Change title, description and cover image
-    $('#title').text(clickedId.charAt(0).toUpperCase() + clickedId.slice(1));
-    $('#description').text(descriptions[clickedId]);
-    // changing background - doesn't work
-    //$(this).find('.jumbotron').css('background', 'linear-gradient(rgba(0, 0, 0, 0.20), rgba(0, 0, 0, 0.20)), url(' + images[clickedId] + ')');
+const router = new ClientRouter();
+
+router
+    .on('category/:category', (params) => load(params));
+
+//$(window).on('load', () => console.log(router._routes));
+$(window).on('hashchange', () => router.navigate());
+
+var load = function(params) {
+    let name = params.category;
+    const url = '/api/items?category=' + name;
+    console.log(url);
+    if (name === '') {
+        name = 'explore projects'
+    }
+    // Update title, description and cover image
+    $('#title').text(name.charAt(0).toUpperCase() + name.slice(1));
+    $('#description').text(descriptions[name]);
     $('.jumbotron').css({
-        'background': 'linear-gradient(rgba(0, 0, 0, 0.20), rgba(0, 0, 0, 0.20)),url(' + images[clickedId] + ')',
+        'background': 'linear-gradient(rgba(0, 0, 0, 0.20), rgba(0, 0, 0, 0.20)),url(' + images[name] + ')',
         'background-repeat': 'repeat',
         'background-position': 'center center, center',
         'color': '#FFF'
     });
 
-    if (clickedId === 'explore') {
-        load('/api/items?category=');
-    } else if (clickedId === 'medical') {
-        load('/api/items?category=medical');
-    } else if (clickedId === 'animals') {
-        load('/api/items?category=animals');
-    } else if (clickedId === 'culture') {
-        load('/api/items?category=culture');
-    } else if (clickedId === 'other') {
-        load('/api/items?category=other');
-    }
-});
-
-var load = function(url) {
+    // Get processed html and insert it in place of old html
     requester.get(url)
         .then(updateHTML)
         .catch((err) => {
--- static/js/router.js
+++ static/js/router.js
@@ -1,5 +1,3 @@
-/* globals $ requester*/
-
 class ClientRouter {
     constructor() {
         this._routes = [];
@@ -44,7 +42,6 @@ class ClientRouter {
         for (var i = 0; i < len; i++) {
             if (targetUrlParts[i][0] !== ':') {
                 if (currentUrlParts[i] !== targetUrlParts[i]) {
-                    console.log('--- NO MATCH ---');
                     return false;
                 }
             } else {
@@ -53,62 +50,7 @@ class ClientRouter {
                 params[paramName] = currentUrlParts[i];
             }
         }
-        console.log('params');
-        console.log(params);
+
         return params;
     }
 }
-
-var descriptions = {
-    'explore projects': 'Find projects which you can help now',
-    medical: 'Donate to alleviate suffering and restore lives of adults and children',
-    animals: 'Save cute pets or endangered species from suffering',
-    culture: 'Support art, social and cultural events or individuals',
-    other: 'Everything else you can be a hero for'
-}
-
-var images = {
-    'explore projects': '../static/images/children-bubbles.jpg',
-    medical: '../static/images/climber-sea.jpg',
-    animals: '../static/images/Climbers-shadow.jpg',
-    culture: '../static/images/FacetheSunShadowCseeker.jpg',
-    other: '../static/images/family-shadow-sea.jpg'
-}
-
-var load = function(params) {
-    let name = params.category;
-    const url = '/api/items?category=' + name;
-    console.log(url);
-    if (name === '') {
-        name = 'explore projects'
-    }
-    // Update title, description and cover image
-    $('#title').text(name.charAt(0).toUpperCase() + name.slice(1));
-    $('#description').text(descriptions[name]);
-    $('.jumbotron').css({
-        'background': 'linear-gradient(rgba(0, 0, 0, 0.20), rgba(0, 0, 0, 0.20)),url(' + images[name] + ')',
-        'background-repeat': 'repeat',
-        'background-position': 'center center, center',
-        'color': '#FFF'
-    });
-
-    // Get processed html and insert it in place of old html
-    requester.get(url)
-        .then(updateHTML)
-        .catch((err) => {
-            console.log('--- GET URL ERROR ---' + err);
-        })
-};
-
-var updateHTML = function(html) {
-    $('#projects').html(html);
-};
-
-
-const router = new ClientRouter();
-
-router
-    .on('category/:category', (params) => load(params));
-
-//$(window).on('load', () => console.log(router._routes));
-$(window).on('hashchange', () => router.navigate());
