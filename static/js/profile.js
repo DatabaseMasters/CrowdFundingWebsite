847222b141d1fe7a04382fcf74a59aa9ef98f88d
ikonomov17 <aleksandar.ikonomov@abv.bg>
2017-07-29 20:05:23 +0300

    Added favourites and user projects in profile

--- static/js/profile.js
+++ static/js/profile.js
@@ -21,9 +21,3 @@ function updateProfile() {
             $('#myModal').modal();
         });
 }
-
-function getCookie(name) {
-    var value = "; " + document.cookie;
-    var parts = value.split("; " + name + "=");
-    if (parts.length == 2) return parts.pop().split(";").shift();
-}
