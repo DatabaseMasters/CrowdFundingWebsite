4764ebbe7e5dd1961025fb15254c534e3812190a
Nadezhda Hristova <epohster@gmail.com>
2017-07-29 22:55:32 +0300

    Created about page (not finished), split server.router and projects.router

--- app/routes/routers.js
+++ app/routes/routers.js
@@ -2,23 +2,12 @@
 const fs = require('fs');
 const path = require('path');
 
-// Dynamic load of all routers in folder
 const attachRoutes = (app, data) => {
-    // TODO: Check if this chaining is ok? Fix 404 not loading
-    app
-        .get('/', (req, res) => {
-            console.log('---- HOME ----');
-            return res.render('home');
-        })
-        .get('/404', (req, res) => {
-            return res.render('404');
-        });
-
+    // Dynamic load of all routers in folder
     fs.readdirSync(__dirname)
         .filter((file) => file.includes('router.js'))
         .map((file) => path.join(__dirname, file))
         .forEach((modulePath) => require(modulePath)(app, data));
-
 };
 
 module.exports = attachRoutes;
--- app/routes/server.router.js
+++ app/routes/server.router.js
@@ -1,66 +1,20 @@
 const { Router } = require('express');
-const login = require('connect-ensure-login');
 
 const attachRoutes = (app, data) => {
     const router = new Router();
 
     router
         .get('/', (req, res) => {
-            res.render('projects/explore');
+            return res.render('home');
         })
-        // form can be shown dynamically
-        // as modal window with javascript - api.router?
-        // TODO delete
-        // .get('/form', (req, res) => {
-        //     return res.render('projects/form');
-        // })
-        .get('/new', login.ensureLoggedIn('/auth/log-in'), (req, res) => {
-            return res.render('projects/new', { model: null });
+        .get('/about', (req, res) => {
+            return res.render('about');
         })
-        .get('/:id', (req, res) => {
-            const id = parseInt(req.params.id, 10);
-            data.projects.getAll({ ref: id })
-                .then((projects) => {
-                    if (!projects || projects.length < 1) {
-                        console.log('----- WRONG PROJECT ID -----');
-                        return res.redirect('/404');
-                    }
-                    return res.render('projects/details', {
-                        model: projects[0],
-                    });
-                })
-                .catch((err) => {
-                    // Check if this flash works!
-                    req.flash('error', err);
-                });
-        })
-        .post('/', (req, res) => {
-            const project = req.body;
-            const file = req.file;
-
-            if (!file) {
-                return res.render('projects/new', { model: project });
-            }
-
-            project.username = req.user.username;
-            project.coverImg = '/' + file.path;
-
-            data.projects.getNextProjectRef()
-                .then((ref) => {
-                    project.ref = ref;
-                    return project;
-                })
-                .then((proj) => data.projects.create(proj))
-                .then((proj) => {
-                    return res.status(201).redirect('/projects/' + proj.ref);
-                })
-                .catch((er) => {
-                    req.flash('error', er);
-                    return res.render('projects/new', { model: project });
-                });
+        .get('/404', (req, res) => {
+            return res.render('404');
         });
 
-    app.use('/projects', router);
+    app.use('/', router);
 };
 
 module.exports = attachRoutes;
